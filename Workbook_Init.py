256004dddac059e9b028c8623cd4e08aac174ba2
JonSolow <jssolow+gh@gmail.com>
2019-08-09 03:12:59 -0400

    Clean notebook and add py script

--- Workbook_Init.py
+++ Workbook_Init.py
@@ -12,6 +12,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 get_ipython().run_line_magic('matplotlib', 'inline')
 
+import matplotlib.ticker as ticker
+
 import seaborn as sns
 sns.set_style('whitegrid')
 
@@ -29,6 +31,8 @@ from sklearn import metrics
 from sklearn.linear_model import LinearRegression
 
 import warnings
+
+import datetime
 
 
 # In[2]:
@@ -67,8 +71,6 @@ def zipcode_melt(df):
 
 # In[5]:
 
-
-from statsmodels.tsa.stattools import adfuller
 
 #create a function that will help us to quickly
 def test_stationarity(timeseries, window):
@@ -335,6 +337,31 @@ def format_results(df, sort_by_field=None):
 
 # In[ ]:
 
-
+def sample_plots_by_scn(df, num_graphs, num_per_row, fig_width=16, hspace=0.6):
+    """Print a sample of the data by Parking location, identified with the field SystemCodeNumber
+    Parameters:
+    num_graphs: Number of locations to make graphs for, ordered by appearance in the dataset.
+    
+    num_per_row: Number of columns in subplot.
+    
+    fig_width: Used to adjust the width of the subplots figure.  (default=16)
+    
+    hspace: Used to adjust whitespace between each row of subplots. (default=0.6)"""
+    num_rows = int(np.ceil(num_graphs/num_per_row))
+    fig, axes = plt.subplots(nrows=num_rows, ncols=num_per_row, figsize=(fig_width, num_rows * fig_width/4))
+    fig.subplots_adjust(hspace=hspace)
+    plt.xticks(rotation=45)
+    for i, scn in enumerate(df.SystemCodeNumber.unique()[:num_graphs]):
+        temp_df = df[df.SystemCodeNumber==scn]
+        ax = axes[i//num_per_row, i%num_per_row]
+        ax.plot(temp_df.LastUpdated, temp_df.PercentOccupied)
+        ax.set_title('Parking Area: {}'.format(scn))
+        ax.set_xlabel('Date')
+        ax.set_ylabel('Percent Occupied')
+        ax.yaxis.set_major_formatter(ticker.PercentFormatter(xmax=1));
+        
+    for ax in fig.axes:
+        plt.sca(ax)
+        plt.xticks(rotation=45)
 
 
